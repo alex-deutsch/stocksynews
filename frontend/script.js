2a1baaa872f95c5491ccf0e9ad6b718337ed252f
Alexander Deutsch <mail@alexanderdeutsch.de>
2018-01-21 06:42:48 +0100

    add logo to news headline

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ function reloadStocksNews() {
 
 	var url = "https://api.iextrading.com/1.0/stock/market/batch";
 	var symbols = "<?php echo getConfigValue('stocksy_stocks'); ?>";
-	var types 	= "news";
+	var types 	= "news,logo";
 	var last 	= "5";
 	var queryURLQuotes = url + "?symbols=" + symbols + "&types=" + types + "&last=" + last;
 	
@@ -17,12 +17,13 @@ function reloadStocksNews() {
 				for(key in data){
 					var i = 0;
 						var news = data[key].news;
-
+						var logo = data[key].logo;
 						$.each(news, function() {
 							if (i < 3){
-								$("#stocks_news_table").append("<tr width='100%'></tr>");
-								$("#stocks_news_table tr:last").append("<td>" + this.headline + "</td>");
-							i++;
+								$("#stocks_news_table").append("<tr></tr>");
+								$("#stocks_news_table tr:last").append("<td><img height='20' src='" + logo.url + "'></img></td>");
+								$("#stocks_news_table tr:last").append("<td width='100%'>" + this.headline + "</td>");
+							i++; 
 							}
 						});
 						j++;
